ddd9844a105440f1e6ec4c47772a978d5b5a4146
M0shiurvau <69873486+M0shiurvau@users.noreply.github.com>
2020-08-19 03:50:04 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+apt install python -y
+apt install python2 -y
+apt install ruby -y
+gem install lolcat
+echo '
+ ==============================================
+|             python3 ipinfo.py                |
+ =============================================='
